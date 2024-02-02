f03c52d4d19c2d2e5d432e2440289e990ca5cb48
Mani_Dev <dhingramsp@Gmail.com>
2024-02-02 23:21:16 +0530

    Optimised code and changed layout

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,53 @@ import './style.css'
 
 const elements = [...document.querySelectorAll('.element')] as HTMLElement[];
 const cards = [...document.querySelectorAll('.card')] as HTMLElement[];
-const body = document.querySelector('html') as HTMLElement;
+const html = document.querySelector('html') as HTMLElement;
+const container = document.querySelector('.full-container') as HTMLElement;
+
 const infoContainer = document.querySelector('.info-container') as HTMLElement;
 const info = document.querySelector('.info') as HTMLElement;
-const indicatorsEl = document.querySelector('.indicators') as HTMLElement;
-const indicatorsButtonEl = document.querySelector('.indicators-button') as HTMLElement;
-const indicators = [...document.querySelectorAll('.indicators span')] as HTMLElement[];
 
-let indicatorIndex = 0
-indicators.forEach(element => {
-	if (indicatorIndex < 4) {
-		element.style.boxShadow = `0 0 calc(0.5 * var(--scale)) calc(0.75 * var(--scale)) var(--state-${element.dataset.name})`;
+const indicatorsContainer = document.querySelector('.indicators') as HTMLElement;
+const indicators = [...document.querySelectorAll('.indicators span')] as HTMLElement[];
+const indicatorsButton = document.querySelector('.indicators-button') as HTMLElement;
+
+// Prevent conventional scrolling
+container.addEventListener("mousewheel", (e) => {
+	e.preventDefault();
+	e.stopPropagation();
+}, false);
+container.addEventListener("DOMMouseScroll", (e) => {
+	e.preventDefault();
+	e.stopPropagation();
+}, false);
+
+document.body.onmousedown = function(e) { if (e.button === 1) return false; }
+
+// Scroll down to indicators when indicator button is clicked
+indicatorsButton.addEventListener('click', () => {
+	indicatorsContainer.scrollIntoView()
+})
+
+indicators.forEach((indicator) => {
+	// add box shadow to indicators, based on state for the first 4, and category for the rest 
+	if (indicator.classList.contains('state-indicator')) {
+		indicator.style.boxShadow = `0 0 calc(0.5 * var(--scale)) calc(0.75 * var(--scale)) var(--state-${indicator.dataset.name})`;
 	} else {
-		element.style.boxShadow = `0 0 calc(0.5 * var(--scale)) calc(0.75 * var(--scale)) var(--type-${element.dataset.name})`;
+		indicator.style.boxShadow = `0 0 calc(0.5 * var(--scale)) calc(0.75 * var(--scale)) var(--type-${indicator.dataset.name})`;
+		indicator.addEventListener('click', () => {
+			html.scroll(0, 0);
+			setTimeout(() => {
+				highlightCategory(indicator.dataset.name as string);
+			}, 1);
+		});
 	}
-	indicatorIndex++;
 });
 
-let infoChildren = [...info.querySelectorAll('*')] as HTMLElement[];
-let extraInfo = [...infoContainer.querySelectorAll('.info-container > p')] as HTMLElement[];
+const infoChildren = [...info.querySelectorAll('*')] as HTMLElement[];
+const extraInfo = [...infoContainer.querySelectorAll('.info-container > p')] as HTMLElement[];
 
 const showInfo = (elementIndex: any) => {
-	hideIndicators();
-	let element = elements[elementIndex - 1];
+	let element = elements[elementIndex];
 	infoContainer.removeAttribute('hidden');
 	infoContainer.style.backgroundColor = `var(--type-${element.dataset.type})`;
 	infoContainer.style.boxShadow = `0 0 calc(0.5 * var(--scale)) calc(0.5 * var(--scale)) black`;
@@ -60,27 +84,33 @@ const hideInfo = () => {
 	extraInfo[1].innerHTML = '';
 }
 
-
-const hideIndicators = () => {
-	indicatorsButtonEl.innerHTML = 'Show Indicators';
-	indicatorsEl.setAttribute('hidden', '');
-}
-const showIndicators = () => {
-	indicatorsButtonEl.innerHTML = 'Hide Indicators';
-	indicatorsEl.removeAttribute('hidden');
+const resetEls = (array: HTMLElement[]) => {
+	array.forEach(e => {
+		e.style.opacity = '1';
+		e.style.transform = 'scale(1)';
+		e.style.filter = 'brightness(1)';
+	})
 }
 
-indicatorsButtonEl.addEventListener('click', () => {
-	if (indicatorsEl.hasAttribute('hidden')) {
-		showIndicators();
-	} else {
-		hideIndicators();
-	}
-});
+const highlightCategory = (currentType: string) => {
+			//Highlight all elements
+			resetEls(elements);
+			//If an element is not of the same category as the one selected, unhighlight it
+			elements.forEach(e => {
+				if (e.dataset.type != currentType) {
+					e.style.opacity = '0.5';
+				}
+			})
+			cards.forEach(e => {
+				if (e.dataset.type != currentType) {
+					e.style.opacity = '0.5';
+				}
+			})
+}
 
 let i = 0
 let j = 0
-body.addEventListener('click', () => {
+html.addEventListener('click', () => {
 	if (!infoContainer.hasAttribute('hidden')) {
 		if (i < 1) {
 			i++
@@ -91,35 +121,25 @@ body.addEventListener('click', () => {
 		j = 0;
 	}
 
-	if (clickedIndex != 0 && infoContainer.hasAttribute('hidden')) {
-		if (j < 1) {
+	if (infoContainer.hasAttribute('hidden')) {
+		if (j < 1 && clickedIndex != -1) {
 			j++
 		} else {
-			elements.forEach(e => {
-				e.style.opacity = '1';
-				e.style.transform = 'scale(1)';
-				e.style.filter = 'brightness(1)';
-			})
-			cards.forEach(e => {
-				e.style.opacity = '1';
-				e.style.transform = 'scale(1)';
-				e.style.filter = 'brightness(1)';
-			})
-			clickedIndex = 0;
+			resetEls(elements);
+			resetEls(cards);
+			clickedIndex = -1;
 			clickedIndexType = '';
 			j = 0;
 		}
 	}
 })
 
-let index = 1;
-let clickedIndex = 0;
+let clickedIndex = -1;
 let clickedIndexType = '';
-elements.forEach(element => {
-	let currentIndex = index;
+elements.forEach((element, index) => {
 	let currentIndexType = element.dataset.type as string;
-	element.classList.add(`element-${index}`);
-	element.dataset.index = `${index}`;
+	element.classList.add(`element-${index + 1}`);
+	element.dataset.index = `${index + 1}`;
 
 	//Color
 	element.style.color = `var(--state-${element.dataset.state})`;
@@ -131,45 +151,28 @@ elements.forEach(element => {
 		if (!infoContainer.hasAttribute('hidden')) {
 			return
 		}
-		if (clickedIndex == currentIndex) {
-			clickedIndex = 0;
-			elements.forEach(e => {
-				e.style.opacity = '1';
-				e.style.transform = 'scale(1)';
-			})
-			cards.forEach(e => {
-				e.style.opacity = '1';
-				e.style.transform = 'scale(1)';
-			})
+		if (clickedIndex == index) {
+			clickedIndex = -1;
+			resetEls(elements);
+			resetEls(cards);
 		}
 
 		if (clickedIndexType == currentIndexType) {
 			clickedIndexType = '';
-			elements[clickedIndex - 1].style.transform = 'scale(1)';
-			elements[clickedIndex - 1].style.filter = 'brightness(1)';
+			elements[clickedIndex].style.transform = 'scale(1)';
+			elements[clickedIndex].style.filter = 'brightness(1)';
 			showInfo(clickedIndex);
-			clickedIndex = currentIndex;
+			clickedIndex = index;
 
 		} else {
 			//If the category of element being selected is not already selected by another element, change the category highlighted
 
-			//Highlight all elements
-			elements.forEach(e => {
-				e.style.opacity = '1';
-				e.style.transform = 'scale(1)';
-				element.style.filter = 'brightness(1)';
-			})
-			//If an element is not of the same category as the one selected, unhighlight it
-			let currentType = element.dataset.type;
-			elements.forEach(e => {
-				if (e.dataset.type != currentType) {
-					e.style.opacity = '0.5';
-				}
-			})
+			let currentType = element.dataset.type as string;
+			highlightCategory(currentType);
 
-			if (clickedIndex != 0) {
-				elements[clickedIndex - 1].style.transform = 'scale(1)';
-				elements[clickedIndex - 1].style.filter = 'brightness(1)';
+			if (clickedIndex != -1) {
+				elements[clickedIndex].style.transform = 'scale(1)';
+				elements[clickedIndex].style.filter = 'brightness(1)';
 			}
 		}
 
@@ -177,7 +180,7 @@ elements.forEach(element => {
 		element.style.filter = 'brightness(1.2)';
 
 		clickedIndexType = currentIndexType;
-		clickedIndex = currentIndex;
+		clickedIndex = index;
 
 		// unhighlights lanthanoid and actinoid cards
 		cards.forEach(e => {
@@ -189,37 +192,33 @@ elements.forEach(element => {
 	});
 
 	element.addEventListener('mouseenter', () => {
-		if (!infoContainer.hasAttribute('hidden')) {
-			return
-		}
+		// enlarge hovered element
+		element.style.transform = 'scale(1.25)';
+		// If no element is selected, highlight hovered element
 		if (clickedIndexType == '') {
 			elements.forEach(e => {
-				if (e.dataset.index == `${currentIndex}`) {
+				if (e.dataset.index == `${index + 1}`) {
 					e.style.filter = 'brightness(1.2)'
 				}
 			});
 		}
-		element.style.transform = 'scale(1.25)';
 	});
 
 	element.addEventListener('mouseleave', () => {
-		if (!infoContainer.hasAttribute('hidden')) {
-			return
+		// When element is no longer hovered, reset brightness
+		element.style.filter = 'brightness(1)';
+
+		// If any element is selected, keep it highlighted
+		if (clickedIndexType != '') {
+			elements[clickedIndex].style.filter = 'brightness(1.2)';
 		}
-		if (clickedIndexType == '') {
-			elements.forEach(e => {
-				e.style.filter = 'brightness(1)';
-			});
-		} else {
-			elements[clickedIndex - 1].style.filter = 'brightness(1.2)';
-		}
-		if (clickedIndex != currentIndex) {
+
+		// Reset size of all elements except the selected one
+		if (clickedIndex != index) {
 			element.style.transform = 'scale(1)';
 		}
 
 	});
-
-	index++
 });
 
 //Lanthanoids
